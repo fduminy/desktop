5d5bed569481301c05c745caf3483d7569aaf9bf
Fabien DUMINY <fduminy@jnode.org>
2019-11-02 18:13:42 +0100

    Refactoring to fix spotbugs issue with UPM_UNCALLED_PRIVATE_METHOD rule under oraclejdk11/openjdk11.

--- src/test/java/fr/duminy/desktop/core/DefaultDesktopDemo.java
+++ src/test/java/fr/duminy/desktop/core/DefaultDesktopDemo.java
@@ -34,41 +34,7 @@ public class DefaultDesktopDemo {
     }
 
     final void init() {
-        desktop = execute(new GuiQuery<DefaultDesktop>() {
-            @Override protected DefaultDesktop executeInEDT() {
-                JPanel content = new JPanel(new BorderLayout());
-
-                content.add(buildCommandPanel(), WEST);
-
-                DefaultDesktop desktop = new DefaultDesktop();
-                content.add(desktop, CENTER);
-
-                JFrame frame = new JFrame("Demo");
-                frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-                frame.setContentPane(content);
-                frame.setMinimumSize(new Dimension(800, 400));
-                frame.pack();
-                frame.setVisible(true);
-
-                Dimension screenSize = getDefaultToolkit().getScreenSize();
-                Dimension size = frame.getSize();
-                frame.setLocation((int) (screenSize.getWidth() - size.getWidth()),
-                                  (int) (screenSize.getHeight() - size.getHeight() - OS_TASK_BAR_SIZE));
-                return desktop;
-            }
-        });
-    }
-
-    private JPanel buildCommandPanel() {
-        JPanel windowCommands = new JPanel(new GridLayout(1, 1));
-        createNewWindowButton(windowCommands);
-        createCloseActiveWindowButton(windowCommands);
-
-        ((GridLayout) windowCommands.getLayout()).setRows(windowCommands.getComponentCount());
-        windowCommands
-            .setBorder(createTitledBorder("<html><center>Window</center><br/><center>commands</center></html>"));
-
-        return windowCommands;
+        desktop = execute(new CreateDesktopQuery());
     }
 
     @VisibleForTesting
@@ -76,39 +42,69 @@ public class DefaultDesktopDemo {
         return desktop;
     }
 
-    private void createNewWindowButton(JPanel parent) {
-        JButton button = new JButton("new window");
-        button.addActionListener(e -> {
-            JInternalFrame window = windowFactory.createWindow();
-            addWindow(window);
-        });
-        parent.add(button);
-    }
+    private class CreateDesktopQuery extends GuiQuery<DefaultDesktop> {
+        @Override
+        protected DefaultDesktop executeInEDT() {
+            JPanel content = new JPanel(new BorderLayout());
 
-    private void createCloseActiveWindowButton(JPanel panel) {
-        JButton result = new JButton("close active window");
-        result.setName("closeActiveFrame");
-        result.addActionListener(e -> {
-            JInternalFrame frame = getSelectedFrame();
-            if (frame == null) {
-                LOG.warn("no active Window to close");
-            } else {
-                try {
-                    frame.setClosed(true);
-                } catch (PropertyVetoException e1) {
-                    fail(e1.getMessage(), e1);
+            content.add(buildCommandPanel(), WEST);
+
+            DefaultDesktop desktop = new DefaultDesktop();
+            content.add(desktop, CENTER);
+
+            JFrame frame = new JFrame("Demo");
+            frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            frame.setContentPane(content);
+            frame.setMinimumSize(new Dimension(800, 400));
+            frame.pack();
+            frame.setVisible(true);
+
+            Dimension screenSize = getDefaultToolkit().getScreenSize();
+            Dimension size = frame.getSize();
+            frame.setLocation((int) (screenSize.getWidth() - size.getWidth()),
+                    (int) (screenSize.getHeight() - size.getHeight() - OS_TASK_BAR_SIZE));
+            return desktop;
+        }
+
+        private JPanel buildCommandPanel() {
+            JPanel windowCommands = new JPanel(new GridLayout(1, 1));
+            createNewWindowButton(windowCommands);
+            createCloseActiveWindowButton(windowCommands);
+
+            ((GridLayout) windowCommands.getLayout()).setRows(windowCommands.getComponentCount());
+            windowCommands
+                    .setBorder(createTitledBorder("<html><center>Window</center><br/><center>commands</center></html>"));
+
+            return windowCommands;
+        }
+
+        private void createNewWindowButton(JPanel parent) {
+            JButton button = new JButton("new window");
+            button.addActionListener(e -> {
+                JInternalFrame window = windowFactory.createWindow();
+                getJDesktopPane(desktop).add(window);
+            });
+            parent.add(button);
+        }
+
+        @SuppressWarnings("ResultOfMethodCallIgnored")
+        private void createCloseActiveWindowButton(JPanel panel) {
+            JButton result = new JButton("close active window");
+            result.setName("closeActiveFrame");
+            result.addActionListener(e -> {
+                JInternalFrame frame = getJDesktopPane(desktop).getSelectedFrame();
+                if (frame == null) {
+                    LOG.warn("no active Window to close");
+                } else {
+                    try {
+                        frame.setClosed(true);
+                    } catch (PropertyVetoException e1) {
+                        fail(e1.getMessage(), e1);
+                    }
+                    LOG.info("active Window is {}", frame.isClosed() ? "closed" : "not closed !!!");
                 }
-                LOG.info("active Window is {}", frame.isClosed() ? "closed" : "not closed !!!");
-            }
-        });
-        panel.add(result);
-    }
-
-    private JInternalFrame getSelectedFrame() {
-        return getJDesktopPane(desktop).getSelectedFrame();
-    }
-
-    private void addWindow(JInternalFrame window) {
-        getJDesktopPane(desktop).add(window);
+            });
+            panel.add(result);
+        }
     }
 }
