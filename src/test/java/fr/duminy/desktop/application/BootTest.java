4070afb0f4e547eb28feb3650d171902ef66163f
Fabien DUMINY <fduminy@jnode.org>
2019-11-03 17:17:53 +0100

    Increase test coverage.

---
+++ src/test/java/fr/duminy/desktop/application/BootTest.java
@@ -0,0 +1,27 @@
+package fr.duminy.desktop.application;
+
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.assertj.swing.core.ComponentFinder;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import javax.swing.*;
+
+import static org.assertj.swing.core.BasicComponentFinder.finderWithNewAwtHierarchy;
+
+@ExtendWith(SoftAssertionsExtension.class)
+class BootTest {
+    private final SoftAssertions softly = new SoftAssertions();
+
+    @Test
+    void main() {
+        ComponentFinder finder = finderWithNewAwtHierarchy();
+
+        Boot.main(new String[0]);
+
+        JFrame frame = finder.findByType(JFrame.class);
+        softly.assertThat(frame).extracting(JFrame::getTitle).isEqualTo("Desktop");
+        softly.assertThat(frame).extracting(JFrame::isVisible).isEqualTo(true);
+    }
+}
--- src/test/java/fr/duminy/desktop/architecture/ArchitectureTest.java
+++ src/test/java/fr/duminy/desktop/architecture/ArchitectureTest.java
@@ -3,26 +3,25 @@ package fr.duminy.desktop.architecture;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import fr.duminy.desktop.application.Boot;
 
 import static com.tngtech.archunit.base.DescribedPredicate.not;
-import static com.tngtech.archunit.core.domain.JavaClass.Predicates.equivalentTo;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAnyPackage;
 import static com.tngtech.archunit.lang.conditions.ArchConditions.resideOutsideOfPackages;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 import static com.tngtech.archunit.library.Architectures.onionArchitecture;
 
 @SuppressWarnings("unused")
 @AnalyzeClasses(packages = "fr.duminy.desktop")
-public class ArchitectureTest {
+class ArchitectureTest {
     @ArchTest
-    public static final ArchRule ARCHITECTURE = onionArchitecture()
+    private static final ArchRule ARCHITECTURE = onionArchitecture()
             .domainModels("fr.duminy.desktop.domain.model..")
             .domainServices("fr.duminy.desktop.domain.service..")
             .applicationServices("fr.duminy.desktop.application.service..")
             .adapter("swingUI", "fr.duminy.desktop.adapter.swing..");
 
     @ArchTest
-    public static final ArchRule PACKAGES = noClasses().that(not(equivalentTo(Boot.class)).and(not(equivalentTo(ArchitectureTest.class))))
+    private static final ArchRule PACKAGES = noClasses().that(not(resideInAnyPackage("fr.duminy.desktop.application", "fr.duminy.desktop.architecture")))
             .should(resideOutsideOfPackages(
                     "fr.duminy.desktop.domain.model..",
                     "fr.duminy.desktop.domain.service..",
